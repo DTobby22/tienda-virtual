5e4f6b1e4315b9750e64a9f42475525db39fc0bf
Daniel <dtobby22@gmail.com>
2023-08-07 22:38:15 -0500

    Creacion del modelo Servicio

--- ServiciosApp/models.py
+++ ServiciosApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Servicio(models.Model):
+    titulo = models.CharField(max_length=30)
+    contenido = models.TextField(blank=True)
+    imagen = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "servicio"
+        verbose_name_plural = "servicios"
+
+    def __str__(self):
+        return self.titulo
+
+
