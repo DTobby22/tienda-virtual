47cf828c410e14612c5becf048ef63d91971361d
Daniel <dtobby22@gmail.com>
2023-08-07 19:21:51 -0500

    Creacion de app Servicios

--- TiendaVirtualApp/urls.py
+++ TiendaVirtualApp/urls.py
@@ -4,8 +4,8 @@ from TiendaVirtualApp import views
 
 urlpatterns = [    
     path('', views.home, name='Home'),
-    path('servicios', views.servicios, name='Servicios'),
-    path('tienda', views.tienda, name='tienda'),
-    path('blog', views.blog, name='Blog'),
-    path('contacto', views.contacto, name='Contacto'),
+    path('servicios', views.servicios, name = 'Servicios'),
+    path('tienda', views.tienda, name = 'Tienda'),
+    path('blog', views.blog, name = 'Blog'),
+    path('contacto', views.contacto, name = 'Contacto'),
 ]
