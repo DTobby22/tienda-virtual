a7479e1bec24e4223dcf7fb4be4555c6ca67bd14
Daniel <dtobby22@gmail.com>
2023-08-13 15:26:40 -0500

    Configurando views de ServiciosApp

--- TiendaVirtualApp/views.py
+++ TiendaVirtualApp/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from ServiciosApp.models import Servicio
+
 
 # Create your views here.
 
 def home(request):
     return render(request, "TiendaVirtualApp/home.html")
-
-def servicios(request):
-    servicio = Servicio.objects.all()
-    return render(request, "TiendaVirtualApp/servicios.html", {"servicios": servicio})
 
 def tienda(request):
     return render(request, "TiendaVirtualApp/tienda.html")
