4ef2455eae613417bf4fd9ed90060b0678f64d73
Daniel <dtobby22@gmail.com>
2023-08-27 17:09:03 -0500

    Configurando los modelos

--- BlogApp/admin.py
+++ BlogApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Post
 
 # Register your models here.
+
+# Aqui se configura el panel de administracion para usar los modelos del blog
+
+class CategoriaAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Post, PostAdmin)
